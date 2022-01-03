c5c4e4095e483db540ff235a72a5af206c5a0185
kell0gg <kell0g_g@naver.com>
2022-01-03 23:54:05 +0900

    220103

---
+++ src/main/kotlin/palindrome_number_09/Solution.kt
@@ -0,0 +1,20 @@
+package palindrome_number_09
+
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        val convertedValue = x.toString()
+        var left: Int = 0
+        var right: Int = convertedValue.length - 1
+
+        while (left < right) {
+            if (convertedValue.get(left++) != convertedValue.get(right--)) return false
+        }
+        return true
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution();
+    val test: Boolean = solution.isPalindrome(-121)
+    print(test)
+}
