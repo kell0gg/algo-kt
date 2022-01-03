348cf341261c1d7c01df0753902146ffff130b61
kell0gg <kell0g_g@naver.com>
2022-01-03 23:44:46 +0900

    220103

--- src/main/kotlin/two_sum_01/Solution.kt
+++ src/main/kotlin/two_sum_01/Solution.kt
@@ -1,21 +1,23 @@
 package two_sum_01
 
 class Solution {
+
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0 until nums.size) {
-            for (j in i + 1 until nums.size) {
-                val tmp: Int = nums[i] + nums[j]
-                if (tmp == target) {
-                    return intArrayOf(i, j)
-                }
-            }
+        val map: MutableMap<Int, Int> = mutableMapOf()
+        nums.forEachIndexed{index, value ->
+            val diff = target - value
+
+            // !! will throw NullPointerException if the value is null
+            if(map.containsKey(diff)) return intArrayOf(map[diff]!!, index)
+            map[value] = index
         }
         return intArrayOf(-1, -1)
     }
 }
+
 fun main(args: Array<String>) {
     val solution = Solution();
-    val nums: IntArray = intArrayOf(1,2,3,4,5)
+    val nums: IntArray = intArrayOf(1, 2, 3, 4, 5)
     val target = 5;
     var a = solution.twoSum(nums, target);
     print(a[0])
