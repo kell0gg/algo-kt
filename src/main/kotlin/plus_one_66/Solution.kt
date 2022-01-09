21946625e8def743214c8bb25f08f3bc98878c2e
kell0gg <kell0g_g@naver.com>
2022-01-09 23:06:04 +0900

    210109

---
+++ src/main/kotlin/plus_one_66/Solution.kt
@@ -0,0 +1,23 @@
+package plus_one_66
+
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+
+        for (i in digits.size - 1 downTo 0) {
+            digits[i] += 1
+            if (digits[i] < 10) return digits
+            digits[i] = 0
+        }
+
+        val arr = IntArray(digits.size + 1)
+        arr[0] = 1
+
+        return arr
+    }
+}
+
+fun main() {
+    var digits: IntArray = intArrayOf(9)
+    var solution = Solution()
+    solution.plusOne(digits)
+}
