3c8e4b7a1abb5dbef732506e6a0bf2fa179a1791
kell0gg <kell0g_g@naver.com>
2022-01-14 00:46:05 +0900

    220114

--- src/main/kotlin/find_single_number_136/Solution.kt
+++ src/main/kotlin/find_single_number_136/Solution.kt
@@ -1,18 +1,22 @@
 package find_single_number_136
 
-
 class Solution {
     fun singleNumber(nums: IntArray): Int {
-//        val map: Map<Int, Int> = HashMap()/*
-//        
-//        nums.forEach { map. }
-//
-//        val map: MutableMap<Int, Int> = HashMap()*/
 
-//        for (i in 0 until nums.length) {
-//            if (map.containsKey(nums[i])) {
-//                map[nums[i]] = map[nums[i]]!! + 1
-//            } else map[nums[i]] = 1
-//        }
+        val map: MutableMap<Int, Int> = HashMap()
+        nums.forEach { map.put(it, map.getOrDefault(it, 0) + 1) }
+
+        for(i in map.entries){
+            if(i.value==1){
+                return i.key
+            }
+        }
+
+        return -1
     }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    solution.singleNumber(intArrayOf(1,2,3))
 }
---
+++ src/main/kotlin/two_sum_2_167/Solution.kt
@@ -0,0 +1,33 @@
+package two_sum_2_167
+
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var left = 0
+        var right = numbers.size - 1;
+
+        while (left < right) {
+            var _tmpSum = numbers[left] + numbers[right]
+            if (_tmpSum == target) {
+                return intArrayOf(left + 1, right + 1)
+            }
+            if (_tmpSum > target) {
+                right--
+            }
+            if (_tmpSum < target) {
+                left++
+            }
+        }
+
+        // not reached
+        return intArrayOf(-1,-1)
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    val result: IntArray = solution.twoSum(intArrayOf(1,2,3,4,5), 4)
+
+    for(i in result){
+        println(i)
+    }
+}
