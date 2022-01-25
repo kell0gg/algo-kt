4e538632e4a57461706b10c0c9e1f774e6bfc159
kell0gg <kell0g_g@naver.com>
2022-01-25 09:00:15 +0900

    220125

---
+++ src/main/kotlin/missing_number_268/Solution.kt
@@ -0,0 +1,13 @@
+package missing_number_268
+
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        var sum = 0
+        for (i in nums.indices) sum += nums[i] - i
+        return nums.size - sum
+    }
+}
+
+fun main(args: Array<String>) {
+    // pass
+}
