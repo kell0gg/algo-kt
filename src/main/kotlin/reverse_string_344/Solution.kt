9c265046c01248dc8a3d9dc9f8b187e5b0a8e727
kell0gg <kell0g_g@naver.com>
2022-01-28 00:59:32 +0900

    220128

---
+++ src/main/kotlin/reverse_string_344/Solution.kt
@@ -0,0 +1,18 @@
+package reverse_string_344
+
+class Solution {
+    fun reverseString(s: CharArray): Unit {
+        // O(1) extra memory
+        for (i in 0..(s.size / 2)) {
+            val tmp = s[i]
+            s[i] = s[s.size - 1 - i]
+            s[s.size - 1 - i] = tmp
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    for (i in 0..(4 / 2)) {
+        print(i)
+    }
+}
