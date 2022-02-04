5b1dc932d03e6c0b200f3e5631aa1015f4b50b48
kell0gg <kell0g_g@naver.com>
2022-02-04 20:24:29 +0900

    220204

---
+++ src/main/kotlin/find_the_difference_389/Solution.kt
@@ -0,0 +1,19 @@
+package find_the_difference_389
+
+class Solution {
+    fun findTheDifference(s: String, t: String): Char {
+
+        val sm: Map<Char, List<Char>> = s.toCharArray().groupBy { it }
+        val tm: Map<Char, List<Char>> = t.toCharArray().groupBy { it }
+
+        for (i in tm) {
+            if (!sm.containsKey(i.key)) {
+                return i.key
+            } else {
+                if (sm.getOrDefault(i.key, arrayListOf()).size != i.value.size) return i.key
+            }
+        }
+
+        return 'e'
+    }
+}
