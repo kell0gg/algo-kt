75b50472d978a7bba16dd3dbd8ae91f6709362da
kell0gg <kell0g_g@naver.com>
2022-02-05 23:02:33 +0900

    220205

---
+++ src/main/kotlin/binary_watch_401/Solution.kt
@@ -0,0 +1,28 @@
+package binary_watch_401
+
+class Solution {
+    fun readBinaryWatch(turnedOn: Int): List<String> {
+
+        // hour: 512, 256, 128, 64
+        // min: 32, 16, 8, 4, 2, 0 ,1
+
+        var answer: MutableList<String> = ArrayList()
+
+        for (i in 0 until 12) {
+            for (j in 0 until 60) {
+                if (Integer.bitCount(i * 64 + j) == turnedOn) {
+                    answer.add(String.format("%d:%02d", i, j))
+                }
+            }
+        }
+        return answer
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    val result = solution.readBinaryWatch(3)
+    for(i in result){
+        println(i)
+    }
+}
